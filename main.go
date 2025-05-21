9aca6c635e8c4e4a89a36de9840dd23243323497
joaopaulomts <jp.nmts@gmail.com>
2025-05-21 00:02:26 -0300

    correção de erros

--- main.go
+++ main.go
@@ -28,8 +28,8 @@ func main() {
 
 	go handleMessages()
 
-	log.Println("HTTP server started on :8080")
-	err := http.ListenAndServe(":8080", nil)
+	log.Println("HTTP server started on :8081")
+	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
